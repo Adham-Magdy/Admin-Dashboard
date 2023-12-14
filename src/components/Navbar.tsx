7e4ec4167ece1b54dd8769890ffd5b5e2db254e0
Adham-Magdy <adhammagdy349@gmail.com>
2023-12-14 15:32:58 +0200

    implement navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,29 @@
+import "../styles/navbar.scss"
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      NavBar
-    </div>
-  )
-}
+      <div className="logo">
+        <img src="/logo.svg" alt="" />
+        <span>Admin Dashboard</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon"/>
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notifications">
+          <img src="/notifications.svg" alt="" className="icon" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="/noavatar.png" alt="" />
+          <span>Adham</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
 
-export default Navbar
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
